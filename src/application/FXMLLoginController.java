67dde8a359c7ee516fa4a76d93ea194d982aa00c
NemanjaMandic <nemanjans84@gmail.com>
2016-01-27 05:04:06 +0100

    finished

--- src/application/FXMLLoginController.java
+++ src/application/FXMLLoginController.java
@@ -5,14 +5,19 @@
  */
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -28,8 +33,20 @@ public class FXMLLoginController implements Initializable {
     @FXML
     private PasswordField txtPassword;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-       
+    private void login(ActionEvent event) throws IOException {
+       if(txtUserName.getText().equals("nemanja") && txtPassword.getText().equals("lozinka")){
+          labelStatus.setText("Login Success !");
+          
+          Stage stage = new Stage();
+           Parent root = FXMLLoader.load(getClass().getResource("FXMLMain.fxml"));
+        
+            Scene scene = new Scene(root);
+        
+            stage.setScene(scene);
+            stage.show();
+       }else{
+           labelStatus.setText("Login Failed !");
+       }
     }
     
     @Override
